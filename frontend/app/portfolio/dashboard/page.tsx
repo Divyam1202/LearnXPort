a0a2efaf3fba01221d46e2cdf4fa3884ea03f293
Divyam1202 <chandakdivyam@gmail.com>
2025-01-01 13:01:36 +0530

    commit

--- frontend/app/portfolio/dashboard/page.tsx
+++ frontend/app/portfolio/dashboard/page.tsx
@@ -428,7 +428,7 @@ export default function DashboardPage() {
               onClick={async () => {
                 logout(); // Clear session
                 await new Promise((resolve) => setTimeout(resolve, 500)); // Wait for session clear
-                window.location.href = "/portfolio/login"; // Redirect to login page
+                window.location.href = "/portfolio/"; // Redirect to login page
               }}
               className="px-4 py-2 text-sm font-medium text-white bg-red-600 rounded-lg"
             >
